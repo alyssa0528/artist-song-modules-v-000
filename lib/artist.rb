01445f8c408f8d7a4cca9603ac1ce5ca75d1aecd
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-03 17:43:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,45 @@
 require 'pry'
 
 class Artist
+  extend Memorable, Findable 
+  include Paramable
+  
   attr_accessor :name
   attr_reader :songs
 
+  #class variable 
   @@artists = []
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
+  
+  #class method that looks for a particular artist in the @@artists array
+  #def self.find_by_name(name)
+   # @@artists.detect{|a| a.name == name}
+  #end
+  
+  #creates new artist and adds that instance to @@artists array. each artist also has a @songs array
   def initialize
     @@artists << self
     @songs = []
   end
-
+  
+  #class method that accesses @@artists 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
-
+  #instance method that takes a song and adds it to the artist's songs array. Associates that song's artist to the current Artist instance 
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
+  #instance method that iterates over @songs array ?
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #instance method that takes artist's name, makes it lowercase, and adds a "-" in any spaces
+  #def to_param
+   # name.downcase.gsub(' ', '-')
+  #end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,35 +1,36 @@
 require 'pry'
 
 class Song
+  extend Memorable, Findable
+  include Paramable
+  
   attr_accessor :name
   attr_reader :artist
 
+  #class variable
   @@songs = []
 
+  #create new Song instances. store each Song instance into @@songs array 
   def initialize
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #class method that searches for a specific song in @@songs 
+  #def self.find_by_name(name)
+   # @@songs.detect{|a| a.name == name}
+  #end
 
+  #artist writer method 
   def artist=(artist)
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  #instance method that takes a song's name, makes it lowercase, and adds a "-" in spaces
+  #def to_param
+   # name.downcase.gsub(' ', '-')
+  #end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
-
+  #class method that accesses @@songs
   def self.all
     @@songs
   end
