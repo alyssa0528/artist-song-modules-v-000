c4a7e9a030e9c87a470ca22e8b2b109ae89f2b1e
Alyssa Kim <alyssa.kim@gmail.com>
2018-03-03 17:47:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,11 +10,6 @@ class Artist
   #class variable 
   @@artists = []
   
-  #class method that looks for a particular artist in the @@artists array
-  #def self.find_by_name(name)
-   # @@artists.detect{|a| a.name == name}
-  #end
-  
   #creates new artist and adds that instance to @@artists array. each artist also has a @songs array
   def initialize
     @@artists << self
@@ -37,9 +32,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  #instance method that takes artist's name, makes it lowercase, and adds a "-" in any spaces
-  #def to_param
-   # name.downcase.gsub(' ', '-')
-  #end
 
 end
